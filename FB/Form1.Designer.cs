cc64f74ac8a15a436266f03e9ada1dad7822d624
Рифмус <kredixlabs@gmail.com>
2017-05-24 19:17:19 +0600

    Начало проекта

---
+++ FB/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace ParserFB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.AccountLogin = new System.Windows.Forms.TextBox();
+            this.AccountPassword = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BoxLog = new System.Windows.Forms.RichTextBox();
+            this.Query = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupslistbox = new System.Windows.Forms.RichTextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ScrollPause = new System.Windows.Forms.TextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Найти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Логин аккаунта";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Пароль аккаунта";
+            // 
+            // AccountLogin
+            // 
+            this.AccountLogin.Location = new System.Drawing.Point(13, 27);
+            this.AccountLogin.Name = "AccountLogin";
+            this.AccountLogin.Size = new System.Drawing.Size(115, 20);
+            this.AccountLogin.TabIndex = 3;
+            this.AccountLogin.Text = "kredix@inbox.ru";
+            // 
+            // AccountPassword
+            // 
+            this.AccountPassword.Location = new System.Drawing.Point(162, 27);
+            this.AccountPassword.Name = "AccountPassword";
+            this.AccountPassword.PasswordChar = '*';
+            this.AccountPassword.Size = new System.Drawing.Size(114, 20);
+            this.AccountPassword.TabIndex = 4;
+            this.AccountPassword.Text = "ET3eD7XJF95TeEXm";
+            // 
+            // panel1
+            // 
+            this.panel1.AccessibleName = "";
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.CausesValidation = false;
+            this.panel1.Controls.Add(this.AccountPassword);
+            this.panel1.Controls.Add(this.AccountLogin);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(6, 15);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(309, 69);
+            this.panel1.TabIndex = 5;
+            // 
+            // BoxLog
+            // 
+            this.BoxLog.Location = new System.Drawing.Point(6, 6);
+            this.BoxLog.Name = "BoxLog";
+            this.BoxLog.Size = new System.Drawing.Size(435, 282);
+            this.BoxLog.TabIndex = 6;
+            this.BoxLog.Text = "";
+            // 
+            // Query
+            // 
+            this.Query.Location = new System.Drawing.Point(18, 27);
+            this.Query.Name = "Query";
+            this.Query.Size = new System.Drawing.Size(129, 20);
+            this.Query.TabIndex = 7;
+            this.Query.Text = "sex";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Поисковый запрос";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(18, 90);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Скроллим";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(2, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(665, 339);
+            this.tabControl1.TabIndex = 10;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.groupslistbox);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.Query);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(657, 313);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Парсим группы";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupslistbox
+            // 
+            this.groupslistbox.Location = new System.Drawing.Point(194, 11);
+            this.groupslistbox.Name = "groupslistbox";
+            this.groupslistbox.Size = new System.Drawing.Size(446, 272);
+            this.groupslistbox.TabIndex = 10;
+            this.groupslistbox.Text = "";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.ScrollPause);
+            this.tabPage2.Controls.Add(this.panel1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(879, 313);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Настройки";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Пауза при скроллинге";
+            // 
+            // ScrollPause
+            // 
+            this.ScrollPause.Location = new System.Drawing.Point(20, 140);
+            this.ScrollPause.Name = "ScrollPause";
+            this.ScrollPause.Size = new System.Drawing.Size(100, 20);
+            this.ScrollPause.TabIndex = 6;
+            this.ScrollPause.Text = "5";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.BoxLog);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(879, 313);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Лог";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(18, 119);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Получить список групп";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 386);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox AccountLogin;
+        private System.Windows.Forms.TextBox AccountPassword;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox BoxLog;
+        private System.Windows.Forms.TextBox Query;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.RichTextBox groupslistbox;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ScrollPause;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ FB/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+
+
+namespace ParserFB
+{
+    public partial class Form1 : Form
+    {
+
+        IWebDriver Browser;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            Browser.Manage().Window.Maximize();
+            BoxLog.Text += "Перешли на Facebook";
+            Browser.Navigate().GoToUrl("https://facebook.com");
+
+            string URL = Browser.Url;
+
+            IWebElement Name = Browser.FindElement( By.Id("email"));
+            IWebElement Password = Browser.FindElement( By.Id("pass"));
+            IWebElement Button = Browser.FindElement(By.Id("loginbutton"));
+         
+            //Вводим данные
+            BoxLog.Text += "\nВводим логин аккаунта";
+            Name.SendKeys(AccountLogin.Text);
+            System.Threading.Thread.Sleep(3000);
+            BoxLog.Text += "\nВводим пароль аккаунта";
+            Password.SendKeys(AccountPassword.Text);
+            System.Threading.Thread.Sleep(3000);
+            BoxLog.Text += "\nКликаем на кнопку Войти";
+            Button.Click();
+            System.Threading.Thread.Sleep(3000);
+
+            //Вводим поисковый запрос
+            System.Threading.Thread.Sleep(3000);
+            BoxLog.Text += "\nВводим поисковый запрос";
+            IWebElement QueryField = Browser.FindElement(By.XPath("//input[@placeholder='Find friends']"));
+            QueryField.SendKeys(Query.Text);
+
+            //Жмем на "найти"
+            System.Threading.Thread.Sleep(3000);
+            BoxLog.Text += "\nКликаем на кнопку Найти";
+            IWebElement SearchButton = Browser.FindElement(By.XPath("//button[@data-testid='facebar_search_button']"));
+            SearchButton.Click();
+
+            //Переходим к списку групп
+            System.Threading.Thread.Sleep(3000);
+            BoxLog.Text += "\nПереходим в группы";
+            IWebElement GroupsList = Browser.FindElement(By.XPath("//a[contains(@href,'/search/groups/')]"));
+            //GroupsList.Click();
+            GroupsList.SendKeys(OpenQA.Selenium.Keys.Control + OpenQA.Selenium.Keys.Shift + OpenQA.Selenium.Keys.Return);
+
+
+        }
+
+        IWebElement GetElement(By locator)
+        {
+            List<IWebElement> elements = Browser.FindElements(locator).ToList();
+            if (elements.Count > 0) return elements[0];
+            else return null;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Скроллим
+            BoxLog.Text += "\nСкроллим";
+            IJavaScriptExecutor js = Browser as IJavaScriptExecutor;
+            System.Threading.Thread.Sleep(5000);
+
+            for (int i = 0; i < 600; i++)
+            {
+                IWebElement AllResultDiv = GetElement(By.XPath("//div[@id='browse_end_of_results_footer']"));
+                if (AllResultDiv == null)
+                {
+                    js.ExecuteScript("window.scrollTo(0, document.body.scrollHeight)");
+                    System.Threading.Thread.Sleep(1000);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<IWebElement> countgroups = Browser.FindElements(By.XPath("//a[contains(@data-testid,'serp_result_link')]")).ToList();
+            
+            for (int cg = 0; cg < countgroups.Count; cg++)
+            {
+                groupslistbox.Text += countgroups[cg].GetAttribute("href")+ "\n";
+            }
+
+        }
+    }
+}
